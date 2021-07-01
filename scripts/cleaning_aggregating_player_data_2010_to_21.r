feddb2eeeb14cc0188afb58fd0ccccd23108825c
AustinStephen <austinstephen98@gmail.com>
2021-07-01 08:24:45 -0600

    touch ups from last commit.

--- scripts/cleaning_aggregating_player_data_2010_to_21.r
+++ scripts/cleaning_aggregating_player_data_2010_to_21.r
@@ -1,7 +1,78 @@
-# PURPOSE: cleanning and gathering data 
+# PURPOSE: cleaning and gathering data on individual players
 # AUTHOR: Austin
 
+library(tidyverse)
+library(dplyr)
+library(nbastatR)
 
-# Player Position Table ---------------------------------------------------
 
+# Constants ---------------------------------------------------------------
+
+years <- c(2010:2021)
+years_smaller <- c("2017-18","2018-19","2019-2020","2020-21")
+
+# Player Position Table Key---------------------------------------------------
+
+## Getting players positions 
+positions_players <- teams_players_stats(seasons= years, 
+                                         types = c("player"), 
+                                         players_positions = c("C", "G", "F") )
+
+
+## initializing an accumulator data frame
+position_id_key <- c()
+
+## binding the results from different years from the teams_players_stats call 
+for(i in 1:24){
+  
+  ## adding season column to interior data
+  positions_players[[7]][[i]] <- positions_players[[7]][[i]] %>% 
+    mutate(season = positions_players[[4]][[i]] ) 
+  
+  ## combining the data into one frame
+  position_id_key <- rbind(position_id_key, 
+                           positions_players[[7]][[i]])
+  
+}
+
+## writing player data including muliple seasons
+position_id_key %>%
+  select(PlayerPosition, idPlayer, namePlayer, season) %>%
+write.csv( ".//data//player_table_all_2010_to_21.csv", 
+          row.names = FALSE)
+
+## writing just IDs, names and positions with no multiple seasons removed 
+position_id_key %>%
+  select(PlayerPosition, idPlayer, namePlayer) %>%
+  distinct(idPlayer,PlayerPosition, .keep_all = TRUE) %>%
+write.csv( ".//data//player_table_reduced_2010_to_21.csv", 
+          row.names = FALSE)
+
+
+# Player Tables with stats -------------------------------------------------
+
+## making a vector of player IDs
+player_id_2017_on <- read.csv(".//data//player_table_all_2010_to_21.csv") %>%
+                      distinct(idPlayer, .keep_all = TRUE) %>%
+                      filter(season %in% years_smaller) 
+player_id_2017_on <- player_id_2017_on[,2]
+
+## getting list of all players names
+dictonary_players <- dictionary_bref_players()
+
+## using players names to get ids (creates df_nba_player_dict automatically)
+nba_player_ids(players = dictonary_players$namePlayerBREF)
+
+## dropping url rows of player names and ids
+df_nba_player_dict <- df_nba_player_dict %>% 
+  select(isActive, isRookie, namePlayer, idPlayer,
+         countSeasons, yearSeasonFirst, yearSeasonLast ) %>%
+  filter(yearSeasonFirst %in% years)
+
+## Must generate the player table for all player of interest in the current dir
+players_tables(player_ids = player_id_2017_on, 
+               tables = c("game splits"),
+               modes = "PerGame" )
+
+write.csv(dataGameSplitsPlayerBase, ".//data//player_game_splits_2017_on.csv")
 
--- testing.r
+++ testing.r
@@ -16,46 +16,6 @@ dictonary_terms <- dictionary_nba_names()
 ## getting list of all players names
 dictonary_players <- dictionary_bref_players()
 
-## using list of players names to get ids
-nba_player_ids(players = dictonary_players$namePlayerBREF)
-
-## dropping extra rows of player names and ids
-df_nba_player_dict <- df_nba_player_dict %>% 
-              select(isActive, isRookie, namePlayer, idPlayer,
-                     countSeasons, yearSeasonFirst, yearSeasonLast )
-
-## getting players profiles  
-testing <- player_profiles( players = "Stephen Curry") %>% 
-      select(idPlayer, idTeam, namePlayer, yearDraft, numberOverallPick, slugTeam,
-             heightInches, weightLBS, pts, ast, treb, ratioPIE )
-
-positions_players <- teams_players_stats(seasons= c(2015,2016,2017,2018,2019,2020) , 
-                                        types = c("player"), 
-                                        players_positions = c("C", "G", "F") )
-
-
-position_id_key <- do.call("rbind", list(positions_players[[7]][[1]],
-                      positions_players[[7]][[2]],
-                      positions_players[[7]][[3]],
-                      positions_players[[7]][[4]],
-                      positions_players[[7]][[5]],
-                      positions_players[[7]][[6]],
-                      positions_players[[7]][[7]],
-                      positions_players[[7]][[8]],
-                      positions_players[[7]][[9]],
-                      positions_players[[7]][[10]],
-                      positions_players[[7]][[11]],
-                      positions_players[[7]][[12]],
-                      positions_players[[7]][[13]],
-                      positions_players[[7]][[14]],
-                      positions_players[[7]][[15]],
-                      positions_players[[7]][[16]],
-                      positions_players[[7]][[17]],
-                      positions_players[[7]][[18]]
-                      )) %>%
-                      select(PlayerPosition, idPlayer, namePlayer)
-
-write.csv(df_nba_player_dict, ".//data//player_names_ids_dictonary.csv")
 # Box Score ---------------------------------------------------------------
 
 box_scores_imported <- box_scores(game_ids = c(21800001:21800005), 
@@ -96,57 +56,14 @@ game_splits <- unnest(player_table_data[11][1],c(dataTable))
 
 
 
-# Looking for systemic relationship with distance traveled -------------------
-if (!require("devtools"))
-  install.packages("devtools")
-devtools::install_github("PatrickChodowski/NBAr",force =TRUE)
-
-library(NBAr)
-
-hustle <- get_hustle(21800001)
-
-## cannot get the function get_shooting() to work 
-
-traditional <- map(c(21800001), ~get_boxscore(.,boxscore_type = 'traditional')) %>% compact() %>%  bind_rows()
-
-## getting the tracking data for 2018 
-tracking_players <- get_tracking(season = c(2018), 
-                        type = c("Player"),
-                        measure_type = c("SpeedDistance"),
-                        game_segment = c(""))
-
-tracking_players <- merge(x = dictonary_players, y= tracking_players, 
-                          by= "Player")
-
-write.csv(tracking_players,"player_tracking_distance_2018.csv")
-
-tracking_teams <- get_tracking(season = c(2018), 
-                      type = c("Team"),
-                      measure_type = c("SpeedDistance"),
-                      game_segment = c("First+Half"),
-                      period = 1)
-
-write.csv(tracking_teams,"team_tracking_distance_2018.csv")
-
-play_by_play <- get_shooting(game_id = 21800001)
-
-
 # Visualizing distance -----------------------------------------------------
-
-## CDF for distance
-tracking_players %>%
-  filter(avg_speed >.25 & avg_speed < 6) %>%
-ggplot(aes(x = avg_speed)) + 
-  stat_ecdf() + 
-  theme_bw() +
-  geom_rug(alpha = .005)+
-  geom_abline( intercept = 0.5, slope = 0, color = "red")+
-  labs(title = "CDF for distance traveled in a game", caption = "")
 
 ## scatter plot distance per game and wins
 ggplot(tracking_teams, aes(x = dist_miles, y= w)) +
     geom_point()+
     theme_bw()
     
+
 summary(tracking_players)
+
 
