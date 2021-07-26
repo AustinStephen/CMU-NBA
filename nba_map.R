8368fe1629a411355f0c28c3eadf73ba8cd1e4b6
calmattyep <matthewyep5170@gmail.com>
2021-07-26 08:04:24 -0700

    added logos to map

--- nba_map.R
+++ nba_map.R
@@ -8,7 +8,7 @@ library(dplyr)
 library(tidyverse)
 
 
-data1415 <- read_csv("/Users/matthewyep/Desktop/Carnegie Mellon/CMU-NBA/data/regseason1415.csv")
+data1415 <- read_csv("/Users/matthewyep/Desktop/CarnegieMellon/CMU-NBA/matthew_data/regseason1415.csv")
 
 
 cumulative_distance1415 <- data1415 %>%
@@ -42,11 +42,11 @@ ggplot() + geom_polygon(data = states, aes(x=long, y = lat, fill = region, group
         panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),
         panel.grid.minor=element_blank(),plot.background=element_blank(),
         legend.position = "bottom") +
-  geom_point(data = highlight, aes(x=Longitude, y=Latitude,colour = "red", size = sqrt(total_distance_traveled))) +
+  geom_point(data = highlight, aes(x=Longitude, y=Latitude, size = sqrt(total_distance_traveled)), color = "red") +
   geom_text(data = coords_and_distance, aes(x=Longitude, y=Latitude, 
                                             label=ifelse(Team %in% c("Portland Trail Blazers","Denver Nuggets", 
                                          "Cleveland Cavaliers", "Boston Celtics"),
-                             as.character(Team),'')),hjust= 1,vjust=0)
+                             as.character(Team),'')),hjust= 0,vjust=0, size = 2.5, color = "blue")
 
   
   #color = Team,
@@ -54,21 +54,53 @@ ggplot() + geom_polygon(data = states, aes(x=long, y = lat, fill = region, group
   #scale_fill_teams(guide = FALSE) +
   #scale_color_teams(4, guide = FALSE)
 
-  
+library(ggimage)
+library(teamcolors)
 
-league_pal("nba")
 
-teamcolors %>%
-  filter(grepl("New ", name)) %>%
-  pull(logo) %>%
-  knitr::include_graphics()
+nba_logos <- filter(teamcolors, league == "nba") %>%
+  select(c("name","logo")) %>%
+  rename(Team = "name")
 
-test <- teamcolors %>%
-  group_by(league) %>%
-  summarize(
-    num_teams = n(),
-    num_logos = sum(!is.na(logo))
-  )
+coords_and_distance <- merge(coords_and_distance, nba_logos,
+                             by = "Team")
+sizes <- c()
+for (d in coords_and_distance$total_distance_traveled) {
+  if (d >60000) {
+    sizes <- c(sizes, 0.12) #99th
+  }
+  if (d<60000 & d> 52000) { 
+    sizes <- c(sizes, 0.1)
+  }
+  if (d<52000 & d> 50000) {
+    sizes <- c(sizes, 0.08)
+  }
+  if (d<50000 & d> 46000) {
+    sizes <- c(sizes, 0.07)
+  }
+  if (d<46000 & d> 43000) {
+    sizes <- c(sizes, 0.06)
+  }
+  if (d<43000 & d> 40000) {
+    sizes <- c(sizes, 0.05)
+  }
+  if (d<40000) {
+    sizes <- c(sizes, 0.04)
+  }
+}
+
+coords_and_distance$img_size <- sizes
+
+ggplot() + geom_polygon(data = states, aes(x=long, y = lat, fill = region, group = group), fill = "white", color = "gray") + 
+  coord_fixed(1.3) + 
+  geom_image(data = coords_and_distance, aes(x = Longitude, y = Latitude, image = logo, size = I(img_size))) +
+  theme(axis.line=element_blank(),axis.text.x=element_blank(),
+        axis.text.y=element_blank(), axis.ticks=element_blank(),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        panel.background=element_blank(),panel.border=element_blank(),panel.grid.major=element_blank(),
+        panel.grid.minor=element_blank(),plot.background=element_blank(),
+        legend.position = "bottom") 
 
 
 # Flight Maps -------------------------------------------------------------
@@ -79,7 +111,7 @@ datos <- nba_travel(start_season = 2015, end_season = 2015)
 nba_travel_plot(data = datos,
                 season = 2015,
                 team = c("Portland Trail Blazers","Denver Nuggets", "Cleveland Cavaliers", "Boston Celtics"),
-                city_color = "white",
+                city_color = "yellow",
                 plot_background_fill = "black",
                 land_color = "gray",
                 caption_color = "lightblue",
